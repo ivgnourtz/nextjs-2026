fa0c9bb22c6bb4d0eb00ee262a1dc5cc09f20f56
vint29 <vipro292002@gmail.com>
2026-03-16 15:57:20 +0700

    style: :sparkles: setup tailwind css variables, add svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,26 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable}  `}
       >
+        <header className="fixed top-0 w-full flex items-center justify-between bg-primary px-8 py-4 shadow-md z-10">
+          <div className="flex items-center space-x-4">
+            <Image src="./icon-apple.svg" alt="Logo" width={40} height={40} />
+            <h1 className="text-2xl font-bold text-white">EnglishAI</h1>
+          </div>
 
+          <div className="flex items-center justify-end">
+            <nav className="flex space-x-6">
+              <Link href="/" className="text-white hover:text-gray-300">
+                Home
+              </Link>
+              <Link href="/about" className="text-white hover:text-gray-300">
+                About
+              </Link>
+              <Link href="/contact" className="text-white hover:text-gray-300">
+                Contact
+              </Link>
+            </nav>
+          </div>
+        </header>
 
         {children}
       </body>
